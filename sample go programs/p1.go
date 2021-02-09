2ef8eed7ada62178f587a645ee46b4de8bc3fb04
Khushei Meghana Meda <43701562+khusheimeda@users.noreply.github.com>
2021-02-09 18:38:47 +0530

    Add files via upload

---
+++ sample go programs/p1.go
@@ -0,0 +1,11 @@
+package main
+
+import ("fmt"
+"math")	//tab space before math doesn't matter
+//or import ("fmt";"math")
+
+func main() {
+	fmt.Println("Welcome to go") 
+	fmt.Println("Line 2")
+	fmt.Println("Math", math.Sqrt(25))	//default sep=' '
+}
