434335f826f0ca61a13d111ec4d0854478fc0e04
Khushei Meghana Meda <43701562+khusheimeda@users.noreply.github.com>
2021-02-14 21:44:41 +0530

    Update p1.go

--- sample go programs/p1.go
+++ sample go programs/p1.go
@@ -1,11 +1,16 @@
 package main
 
-import ("fmt"
-"math")	//tab space before math doesn't matter
+import ("fmt")	//tab space before math doesn't matter
 //or import ("fmt";"math")
 
 func main() {
 	fmt.Println("Welcome to go") 
-	fmt.Println("Line 2")
-	fmt.Println("Math", math.Sqrt(25))	//default sep=' '
+	a := 3.1415E+3
+	qwertyuiopasdfghjklzxcvbnmqwertyu := 2.5 + a
+	fmt.Print(a, b)
 }
+
+/* comment
+
+
+
