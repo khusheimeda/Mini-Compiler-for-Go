e94a1dd1415e1bb186e4e8f7ec70956881328eee
Amrutha U <51371154+amrutha00@users.noreply.github.com>
2021-02-23 23:46:14 +0530

    Create p10.go

---
+++ latest_working/p10.go
@@ -0,0 +1,30 @@
+package main;
+import "fmt";
+
+func main() {
+
+   var i int = 1;
+    
+   var qwertyuiopasdfghjklzxcvbnm123456 float =  3.1415E+3;
+   /* for ;i <= 3; {
+        fmt.Println(i,"\t");
+        i = i + 1;
+    };
+    fmt.Println("\n");
+
+    var k int;
+
+    for k = 7; k <= 9; k++ {
+        fmt.Println(k,"\t");
+    };
+    fmt.Println("\n");
+
+    var n int;
+    for n = 0; n <= 5; n++ {
+        if (n%2 == 0) {
+            continue ;
+        };
+        fmt.Println(n,"a \t ");
+    };
+    fmt.Println("\n");
+}
